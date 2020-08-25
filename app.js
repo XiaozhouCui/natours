469b2f79e21ff56bb9e3d4bd655f63cbdeeca9b1
Joe Cui <joe.cui@bpanz.com>
2020-08-25 23:14:24 +1000

    Added get and post route handlers for /api/v1/tours

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+// middleware to convert json into js object as req.body
+app.use(express.json());
+
+// read data into js object before sending it in responses
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3001;
