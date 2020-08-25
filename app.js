dacf08f47673cc71c4e35950c2f6b688300c1eec
Joe Cui <joe.cui@bpanz.com>
2020-08-25 19:47:25 +1000

    Initial commit: setting up project folder

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+});
+
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
