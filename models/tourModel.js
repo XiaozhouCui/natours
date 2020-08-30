5351baf820bb648a8944e05bc9fdddb8ad5e8505
Joe Cui <joe.cui@bpanz.com>
2020-08-30 11:54:39 +1000

    Updated tour model schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,17 +3,56 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
+    required: [true, 'A tour must have a name'], // error message if undefined
+    unique: true, // no duplicate name allowed
+    trim: true, // remove white space
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficluty'],
+    trim: true,
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  imageCover: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String], // array of strings
+  createdAt: {
+    type: Date,
+    default: Date.now(), // mongoose will auto parse date
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
