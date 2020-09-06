02ff5fb82d7304c0ee7b95c25b81acb51110a01f
Joe Cui <joe.cui@bpanz.com>
2020-09-06 11:42:30 +1000

    Handle global errors outside Express middlewares

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// Handle global exceptions outside Express middlewares (e.g. using undefined varables)
+// This global error handler must be at the top, before app is called
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  process.exit(1);
+});
+
 // updata the process.env with config.env
 dotenv.config({ path: './config.env' });
 // console.log(process.env);
@@ -28,6 +36,17 @@ mongoose
 
 const port = process.env.PORT || 3001; // if not defined, use port 3001;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+// Handle global promise rejections outside Express middlewares (e.g. DB connection problem)
+// This global error handler must be at the bottom, after server listener
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    console.log('Server closed.');
+    process.exit(1);
+  });
 });
