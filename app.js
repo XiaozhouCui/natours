38d16dd18855cc6bb13805cf646635b7aaf33707
Joe Cui <joe.cui@bpanz.com>
2020-09-09 16:25:37 +1000

    Security: rate-limit, sanitisation, safety headers (helmet) and parameter pollution

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -7,18 +13,54 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   // log req and res info
   app.use(morgan('dev'));
 }
 
-// convert json into js object as req.body
-app.use(express.json());
-// serve static files
+// limit number of requests from same IP to 100/hr
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+// call rateLimit() will return a new function as middleware
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitisation against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitisation against XSS
+app.use(xss());
+
+// Prevent parameter pollution (e.g. ?sort=a&sort=b)
+// only use last duplicate param (sort=b)
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Serve static files
 app.use(express.static(`${__dirname}/public`));
 
-// time stamp middleware
+// Time stamp middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
