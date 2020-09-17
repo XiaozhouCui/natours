939fe73d1f6fed51f00e1b43c1d7acabeb699d3f
Joe Cui <joe.cui@bpanz.com>
2020-09-17 18:36:09 +1000

    Added pug base template

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,7 +15,14 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) GLOBAL MIDDLEWARES
+
+// Serve static files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
 app.use(helmet());
@@ -58,9 +66,6 @@ app.use(
   })
 );
 
-// Serve static files
-app.use(express.static(`${__dirname}/public`));
-
 // Time stamp middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -69,6 +74,14 @@ app.use((req, res, next) => {
 });
 
 // 2) ROUTES
+// base route with SSR (base.pug)
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Joe',
+  });
+});
+
 // mount new router as middleware
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
