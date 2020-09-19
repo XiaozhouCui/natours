185e0bed01bf9b2565f3825cc1e24d7fc90398ba
Joe Cui <joe.cui@bpanz.com>
2020-09-19 17:52:12 +1000

    Added overview page using pug

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,28 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) Get data for the requested tour (including reviews and tour guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
