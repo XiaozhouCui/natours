8188fb829619673792fc2e731213ade4c5486ccf
Joe Cui <joe.cui@bpanz.com>
2020-09-25 22:46:01 +1000

    Upload and process multiple Tour Images

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,18 +43,19 @@ exports.uploadUserPhoto = upload.single('photo');
 
 // RESIZE USER PHOTO WITH SHAPR
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-  sharp(req.file.buffer)
+
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
     .toFile(`public/img/users/${req.file.filename}`);
 
   next();
-};
+});
 
 // ROUTE HANDLERS
 
